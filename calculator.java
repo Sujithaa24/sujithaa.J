cab3eded475b5e00642b02647a7653520d9b369f
Sujithaa24 <sujithaa075@gmail.com>
2024-12-16 12:06:22 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 class Biotech{
 void add(int a ,int b)
 {
@@ -22,11 +22,8 @@ System.out.println("multi:"+(a*b));
 class calculator{
 public static void main (String args[])
 {
-Scanner scan = new Scanner(System.in);
-System.out.println("enter numm1:");
-int a = scan.nextInt();
-System.out.println("enter nummm2:");
-int b = scan.nextInt();
+int a = 10
+int b = 5
 Computer cs = new Computer();
 Civil eng = new Civil();
 cs.add(a,b);
